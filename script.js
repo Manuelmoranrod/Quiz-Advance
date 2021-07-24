a6a0f8cd8c32a3103ca961189c35c56555cc364a
Manuelmoranrodriguez <manuel.moran.rodriguez@gmail.com>
2021-07-24 19:22:32 +0100

    draw next q funciona

--- script.js
+++ script.js
@@ -2,61 +2,90 @@
 //https://opentdb.com/api.php?amount=10&category=9&difficulty=hard&type=multiple
 //Pantalla principal en la que se empieza el juego
 const starButton = document.getElementById('start')
-const questionContainerElement = document.getElementById('principal')
+//const questionContainerElement = document.getElementById('principal')
+const question = document.querySelector('div.pregunta p')
+const answer = document.getElementById('btnR')
+const options = document.getElementsByClassName('btn')
 
+
+let responseQuiz = {}
+
+let round = 0
+let score = 0
 
 //Evento listener en pantalla home.html para iniciar juego
 //starButton.addEventListener('click', startGame)
+async function getQuestion() {
+    let contentPreg = await fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=hard&type=multiple')
+    let pregNew = await contentPreg.json()
+    return pregNew
+}
 
 function startGame() {
     console.log('Empieza el juego')
-    let score = 0
-    let round = 0
-    const goNext = document.getElementById('next')
-    
+
     //Peticion API de las preguntas
-    
-    async function getQuestion() {
-        let contentPreg = await fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=hard&type=multiple')
-        let pregNew = await contentPreg.json()
-        return pregNew   
-    }
+
     getQuestion()
-    .then(pregNew => {
-        let apiQuestion = pregNew.results[0].question
-        //console.log(apiQuestion)
-        //const containerPreg = document.getElementsByClassName('pregunta')
+        .then(pregNew => {
+            console.log(pregNew);
+            responseQuiz = pregNew
+            drawNextQ()
+            //pregNew.map()
+        })
+}
+//Comprobacion de las respuestas cuando pulse next
+function goNext() {
+    validateAnswer()
+    drawNextQ()
+    //else{ redirecciona results html y guarda en base de datos}
+}
+
+function drawNextQ() {
+    let apiQuestion = responseQuiz.results[round].question
+    let apiAns = responseQuiz.results[round].correct_answer
+    let apiOpt = responseQuiz.results[round].incorrect_answers
+    question.innerHTML = apiQuestion
+    //answer.innerHTML = apiAns
+    
+    /*for(let i=0; i<=options.lenght; i++){
+        for (let i = 0; i < 11; i++) {
+            postI += i;
+            postJ += i;
+            suma = postJ + postI;
+            console.log(
+            `En la Iteración Nº ${i}: la suma de postI y postJ es: ${suma}`
+            );
+        }
+    };*/
+    options[0].innerHTML = apiOpt[0]
+    options[1].innerHTML = apiOpt[1]
+    options[2].innerHTML = apiOpt[2]
+    //falta pintarle el id para la validacion
+    options[3].innerHTML = apiAns
+    console.log(apiQuestion)
+    console.log(apiAns);
+    console.log(apiOpt);
+    console.log(question);
+    //console.log(answer);
+    console.log(options);
+    round++
+    
+}
+
+function validateAnswer(){
+    let select = document.getElementsByClassName('btn')
+    console.log(select);
+    if(select == "answer"){
+        score++;
+        answer.style.backgroundColor = "green"
         
-
-        containerPreg.innerHTML = apiQuestion
-        //pregNew.map()
-    })
-
-    //Comprobacion de las respuestas cuando pulse next
-
-    goNext.addEventListener('click', selectAnswer)
-
-    function selectAnswer() {
-        if(question.value == true) {
-            score++
-            //pintar de verde la casilla
-        }
-        else{
-            //pintar de rojo la casilla
-        }
-        //add on de round for paint the next question 
-        round++
-        //remove ant question 
-        document.body.removeChild('pregunta')
-        document.body.appendChild('pregunta')
-        if(round === 10){
-            removeChild(question)
-            window.location.assign(results.html)
-        }
-        
+    }else{
+        options.style.backgroundColor = "red"
     }
+    
+}
 
     //guardar en la base de datos de firestore los resultados de las partida
     //y mostrar en una grafica
 
-}
